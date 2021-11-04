8b67c13c2884b41808d796347e3cab6401b8efe4
AndriiRudiuk <andriyr@yozma.tech>
2021-11-04 21:34:10 +0200

    global object

---
+++ global.js
@@ -0,0 +1,12 @@
+// console.log(global)
+
+// setTimeout (() => {
+//   console.log('Hello')
+// }, 2000)
+
+// console.log(__dirname)  путь папки 
+// console.log(__filename) название и путь к файлу
+
+// console.log(process.argv)
+
+// const url = new URL('https://www.site')
